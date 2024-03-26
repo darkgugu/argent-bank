4662501f998be36fcbee13315891ce7e4db37661
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-03-26 15:12:25 +0100

    First Commit

---
+++ argent-bank-front/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { Home } from './pages/Home'
+import { SignIn } from './pages/SignIn'
+import { Account } from './pages/Account'
+import { Header } from './components/Header'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+	<React.StrictMode>
+		<Router>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/signin" element={<SignIn />} />
+				<Route path="/account/:user" element={<Account />} />
+			</Routes>
+		</Router>
+	</React.StrictMode>,
+)
