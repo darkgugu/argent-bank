d1c6df838f11de05f3216fd69a8fed09459d7469
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-03-27 18:51:39 +0100

    Update SignIn.jsx

--- argent-bank-front/src/pages/SignIn.jsx
+++ argent-bank-front/src/pages/SignIn.jsx
@@ -1,5 +1,40 @@
+import { useNavigate } from 'react-router-dom'
 import '../assets/css/main.css'
 
 export const SignIn = () => {
-	return <div className="SignIn">SignIn</div>
+	const navigate = useNavigate()
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		navigate('/account/1')
+	}
+	return (
+		<main className="main bg-dark">
+			<section className="sign-in-content">
+				<i className="fa fa-user-circle sign-in-icon"></i>
+				<h1>Sign In</h1>
+				<form>
+					<div className="input-wrapper">
+						<label htmlFor="username">Username</label>
+						<input type="text" id="username" />
+					</div>
+					<div className="input-wrapper">
+						<label htmlFor="password">Password</label>
+						<input type="password" id="password" />
+					</div>
+					<div className="input-remember">
+						<input type="checkbox" id="remember-me" />
+						<label htmlFor="remember-me">Remember me</label>
+					</div>
+					<button
+						type="submit"
+						class="sign-in-button"
+						onClick={(e) => handleSubmit(e)}
+					>
+						Sign In
+					</button>
+				</form>
+			</section>
+		</main>
+	)
 }
