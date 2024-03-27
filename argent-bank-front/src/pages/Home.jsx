6fced821a6eab7b7aa0a29fb0bfe83fa705e1fed
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-03-27 18:51:43 +0100

    Update Home.jsx

--- argent-bank-front/src/pages/Home.jsx
+++ argent-bank-front/src/pages/Home.jsx
@@ -1,10 +1,47 @@
 import '../assets/css/main.css'
-import { Footer } from '../components/Footer'
+import { HomepageFeatures } from '../components/HomepageFeatures'
+import chatIcon from '../assets/img/icon-chat.png'
+import moneyIcon from '../assets/img/icon-money.png'
+import shieldIcon from '../assets/img/icon-security.png'
 
 export const Home = () => {
 	return (
-		<div className="Home">
-			<Footer></Footer>
-		</div>
+		<main>
+			<div className="hero">
+				<section className="hero-content">
+					<h2 className="sr-only">Promoted Content</h2>
+					<p className="subtitle">No fees.</p>
+					<p className="subtitle">No minimum deposit.</p>
+					<p className="subtitle">High interest rates.</p>
+					<p className="text">
+						Open a savings account with Argent Bank today!
+					</p>
+				</section>
+			</div>
+			<section className="features">
+				<h2 className="sr-only">Features</h2>
+				<HomepageFeatures
+					title="You are our #1 priority"
+					text="Need to talk to a representative? You can get in touch
+						through our 24/7 chat or through a phone call in less
+						than 5 minutes."
+					img={chatIcon}
+					alt="Chat Icon"
+				/>
+				<HomepageFeatures
+					title="More savings means higher rates"
+					text="The more you save with us, the higher your interest rate will be!"
+					img={moneyIcon}
+					alt="Money Icon"
+				/>
+				<HomepageFeatures
+					title="Security you can trust"
+					text="We use top of the line encryption to make sure your data and money
+					is always safe."
+					img={shieldIcon}
+					alt="Shield Icon"
+				/>
+			</section>
+		</main>
 	)
 }
