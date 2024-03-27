a878bec642cbc3211e607fc29048755bca5ca852
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-03-27 18:52:06 +0100

    Add Header & Footer to Router

--- argent-bank-front/src/index.js
+++ argent-bank-front/src/index.js
@@ -5,6 +5,7 @@ import { Home } from './pages/Home'
 import { SignIn } from './pages/SignIn'
 import { Account } from './pages/Account'
 import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
@@ -18,6 +19,7 @@ root.render(
 				<Route path="/signin" element={<SignIn />} />
 				<Route path="/account/:user" element={<Account />} />
 			</Routes>
+			<Footer />
 		</Router>
 	</React.StrictMode>,
 )
