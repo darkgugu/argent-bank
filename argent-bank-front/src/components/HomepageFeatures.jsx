1ac6f81b3eae196bd7fb4a704d98c3437b5817fb
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-03-27 18:51:49 +0100

    Update HomepageFeatures.jsx

--- argent-bank-front/src/components/HomepageFeatures.jsx
+++ argent-bank-front/src/components/HomepageFeatures.jsx
@@ -1,7 +1,11 @@
-import '../style/css/main.css'
+import '../assets/css/main.css'
 
-export const HomepageFeatures = () => {
-
-
-	return <div className='HomepageFeatures'>HomepageFeatures</div>
+export const HomepageFeatures = ({ title, img, alt, text }) => {
+	return (
+		<div className="feature-item">
+			<img src={img} alt={alt} className="feature-icon" />
+			<h3 className="feature-item-title">{title}</h3>
+			<p>{text}</p>
+		</div>
+	)
 }
