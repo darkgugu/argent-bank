e72760d10d76c124686aa241524a52ee05e39ee7
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-04-02 09:42:31 +0200

    Create EditUserInfo.jsx

---
+++ argent-bank-front/src/components/EditUserInfo.jsx
@@ -0,0 +1,37 @@
+import { useStore } from 'react-redux'
+import '../assets/css/main.css'
+import { editUserInfoSlice } from './editUserInfoSlice'
+import { updateUsername } from '../app/store'
+
+export const EditUserInfo = ({ setIsEditing }) => {
+	const store = useStore()
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		const username = document.getElementById('username').value
+		console.log('username :', username)
+		store.dispatch(updateUsername(username))
+		//store.dispatch(editUserInfoSlice.actions.updateUsername(username))
+		setIsEditing(false)
+	}
+
+	const leave = (e) => {
+		e.preventDefault()
+		setIsEditing(false)
+	}
+	return (
+		<form className="EditUserInfo" onSubmit={(e) => handleSubmit(e)}>
+			<label htmlFor="username">User name: </label>
+			<input type="text" name="username" id="username"></input>
+			<br />
+			<label htmlFor="firstname">First name: </label>
+			<input type="text" name="firstname" id="firstname"></input>
+			<br />
+			<label htmlFor="lastname">Last name: </label>
+			<input type="text" name="lastname" id="lastname"></input>
+			<br />
+			<button type="submit">Save</button>
+			<button onClick={(e) => leave(e)}>Cancel</button>
+		</form>
+	)
+}
