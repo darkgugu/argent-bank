2d451122ca2a6b7aaef43fc7be4f2033b10f2a32
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-04-02 09:41:18 +0200

    Add provider to router

--- argent-bank-front/src/index.js
+++ argent-bank-front/src/index.js
@@ -6,20 +6,24 @@ import { SignIn } from './pages/SignIn'
 import { Account } from './pages/Account'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<Router>
-			<Header />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/signin" element={<SignIn />} />
-				<Route path="/account/:user" element={<Account />} />
-			</Routes>
-			<Footer />
-		</Router>
+		<Provider store={store}>
+			<Router>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/signin" element={<SignIn />} />
+					<Route path="/account/:user" element={<Account />} />
+				</Routes>
+				<Footer />
+			</Router>
+		</Provider>
 	</React.StrictMode>,
 )
