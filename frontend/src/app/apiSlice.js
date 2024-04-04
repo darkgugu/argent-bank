e9b179f5a96296c279698c4957e6b1333b21b5d1
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-04-04 16:59:54 +0200

    getProfile (not working)

--- frontend/src/app/apiSlice.js
+++ frontend/src/app/apiSlice.js
@@ -5,11 +5,23 @@ const backendUrl = 'http://localhost:3001/api/v1/'
 export const apiSlice = createApi({
 	reducerPath: 'api',
 	baseQuery: fetchBaseQuery({ baseUrl: backendUrl }),
+	prepareHeaders: (headers, { getState }) => {
+		const token = getState().token
+
+		if (token) {
+			headers.set('authorization', `Bearer ${token}`)
+		}
+
+		return headers
+	},
 	endpoints: (builder) => ({
 		login: builder.mutation({
 			query: (body) => ({ url: 'user/login', method: 'POST', body }),
 		}),
+		getProfile: builder.mutation({
+			query: (body) => ({ url: 'user/profile', method: 'POST', body }),
+		}),
 	}),
 })
 
-export const { useLoginMutation } = apiSlice
+export const { useLoginMutation, useGetProfileMutation } = apiSlice
