36525bc01d5714840c161b0a2393c07965aa2829
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-04-08 19:19:19 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,7 +20,7 @@ root.render(
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/signin" element={<SignIn />} />
-					<Route path="/account/:user" element={<Account />} />
+					<Route path="/account" element={<Account />} />
 				</Routes>
 				<Footer />
 			</Router>
