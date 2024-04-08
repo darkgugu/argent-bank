1fadf2027410d30f194f84bdcf333dd2eae8f8e0
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-04-08 19:20:05 +0200

    Update apiSlice.js

--- frontend/src/app/apiSlice.js
+++ frontend/src/app/apiSlice.js
@@ -2,24 +2,34 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const backendUrl = 'http://localhost:3001/api/v1/'
 
+const getToken = (state) => state?.token
+
 export const apiSlice = createApi({
 	reducerPath: 'api',
-	baseQuery: fetchBaseQuery({ baseUrl: backendUrl }),
-	prepareHeaders: (headers, { getState }) => {
-		const token = getState().token
+	baseQuery: fetchBaseQuery({
+		baseUrl: backendUrl,
+		prepareHeaders: (headers, { getState }) => {
+			const token = getToken(getState())
 
-		if (token) {
-			headers.set('authorization', `Bearer ${token}`)
-		}
+			if (token) {
+				headers.set('authorization', `Bearer ${token}`)
+			} else {
+				console.log('ERROR no token')
+			}
 
-		return headers
-	},
+			return headers
+		},
+	}),
 	endpoints: (builder) => ({
 		login: builder.mutation({
 			query: (body) => ({ url: 'user/login', method: 'POST', body }),
 		}),
 		getProfile: builder.mutation({
-			query: (body) => ({ url: 'user/profile', method: 'POST', body }),
+			query: (body) => ({
+				url: 'user/profile',
+				method: 'POST',
+				body,
+			}),
 		}),
 	}),
 })
