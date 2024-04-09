5b40892b8154f4e523049a8012da5094c1228b84
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-04-09 23:55:52 +0200

    Dynamic header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,21 @@
 import '../assets/css/main.css'
 import logo from '../assets/img/argentBankLogo.png'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { useSelector, useStore } from 'react-redux'
+import { getUserName } from '../app/selectors'
+import { signInSlice } from '../slices/signInSlice'
+import { editUserInfoSlice } from '../slices/editUserInfoSlice'
 
 export const Header = () => {
+	const { pathname } = useLocation()
+	const userName = useSelector(getUserName)
+	const store = useStore()
+
+	const handleSignOut = (e) => {
+		store.dispatch(signInSlice.actions.clearToken())
+		store.dispatch(editUserInfoSlice.actions.clearUser())
+	}
+
 	return (
 		<nav className="main-nav">
 			<Link className="main-nav-logo" to="/">
@@ -14,9 +27,24 @@ export const Header = () => {
 				<h1 className="sr-only">Argent Bank</h1>
 			</Link>
 			<div>
-				<Link className="main-nav-item" to="/signin">
-					<i className="fa fa-user-circle"></i> Sign In
-				</Link>
+				{pathname !== '/account' ? (
+					<Link className="main-nav-item" to="/signin">
+						<i className="fa fa-user-circle"></i> Sign In
+					</Link>
+				) : (
+					<>
+						<Link className="main-nav-item">
+							<i className="fa fa-user-circle"></i> {userName}
+						</Link>
+						<Link
+							className="main-nav-item"
+							to="/"
+							onClick={(e) => handleSignOut(e)}
+						>
+							<i className="fa fa-sign-out"></i> Sign Out
+						</Link>
+					</>
+				)}
 			</div>
 		</nav>
 	)
